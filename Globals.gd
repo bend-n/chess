c3e8b762779e9d992ceccd47eccc01ae31326484
bendn <bend.n@outlook.com>
2022-04-26 17:38:14 +0700

    testing

--- Globals.gd
+++ Globals.gd
@@ -2,7 +2,7 @@ extends Node
 
 var grid: Grid = null
 var turns := 0
-var black_incheck = false
-var white_incheck = false
+var in_check = false
+var checking_piece: Piece = null
 var turn := true  # true for white, false for black
 # true cuz white goes first
