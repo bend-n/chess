b6861cabcba895f1a44473f0a74df1ea5f887018
bendn <bend.n@outlook.com>
2022-04-27 19:49:29 +0700

    h

--- pieces/King.gd
+++ pieces/King.gd
@@ -7,7 +7,7 @@ func get_moves():
 	for i in all_dirs():
 		var spot = pos_around(i)
 		if is_on_board(spot):
-			if check_spots_check and checkcheck(spot):
+			if check_spots_check and Globals.in_check and checkcheck(spot):
 				continue
 			moves.append(spot)
 	return moves
