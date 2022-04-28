d28c3084baa6335182ee274c4d6bfca339d37fc0
bendn <bend.n@outlook.com>
2022-04-28 08:01:19 +0700

    checkmates

--- Globals.gd
+++ Globals.gd
@@ -1,10 +1,25 @@
 extends Node
 
 var grid: Grid = null
-var turns := 0
+var white_turns := 0
+var black_turns := 0
 var in_check = false
 var checking_piece: Piece = null
 var white_king: King
 var black_king: King
 var turn := true  # true for white, false for black
 # true cuz white goes first
+
+
+func turns(winner):
+	if winner == "white":
+		return white_turns
+	elif winner == "black":
+		return black_turns
+
+
+func add_turn():
+	if turn:
+		white_turns += 1
+	else:
+		black_turns += 1
