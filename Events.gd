b3d4dddd86eca876da978f46660f5227b1d76ad4
bendn <bend.n@outlook.com>
2022-05-16 14:02:35 +0700

    fix & disable timers

--- Events.gd
+++ Events.gd
@@ -6,3 +6,4 @@ signal just_before_turn_over  # called just before turn over
 signal outoftime  # called when the time is up
 signal game_over  # called when the game is over
 signal go_back  # called when the game is over, and were ready to go back
+signal data_recieved  # called every time the data comes in
