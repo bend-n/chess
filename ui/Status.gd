d09baa21b289a7bd05c9db583e1f26e85fa66da9
bendn <bend.n@outlook.com>
2022-05-30 08:21:54 +0700

    the signal way

--- ui/Status.gd
+++ ui/Status.gd
@@ -5,5 +5,6 @@ class_name StatusLabel
 func set_text(newtext: String, time := 7) -> void:
 	print("set to ", newtext)
 	text = newtext
-	yield(get_tree().create_timer(time), "timeout")
-	text = ""
+	if time != 0:
+		yield(get_tree().create_timer(time), "timeout")
+		text = ""
