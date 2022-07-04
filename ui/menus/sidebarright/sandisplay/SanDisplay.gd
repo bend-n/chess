8c3996afe54371e53b5ccfc3790353efd9fc0a4e
bendn <bend.n@outlook.com>
2022-07-04 14:28:25 +0700

    Arrow support
    not coke
    they arent red
    i made sure
    wait do coke ads still have the arrow :thinking:

--- ui/menus/sidebarright/sandisplay/SanDisplay.gd
+++ ui/menus/sidebarright/sandisplay/SanDisplay.gd
@@ -16,14 +16,10 @@ func _ready() -> void:
 	scroll_bar.hide()
 	scroll_bar.step = .15  #smoth
 	add_child(tween)
-	if Globals.grid:
-		Globals.grid.connect("add_to_pgn", self, "add_move")
-		Globals.grid.connect("load_pgn", self, "add_moves")
-		Globals.grid.connect("clear_pgn", self, "clear")
-		Globals.grid.connect("remove_last", self, "pop")
-	else:
-		for i in "hello how do you do":
-			add_move(i)
+	Globals.grid.connect("add_to_pgn", self, "add_to_pgn")
+	Globals.grid.connect("load_pgn", self, "add_moves")
+	Globals.grid.connect("clear_pgn", self, "clear")
+	Globals.grid.connect("remove_last", self, "pop")
 
 
 func create_number_label(num: int) -> void:
@@ -34,18 +30,23 @@ func create_number_label(num: int) -> void:
 	base.name = base.number.text
 
 
+func add_to_pgn(m: String) -> void:
+	add_move(m)
+	scroll_down()
+
+
 func add_move(move: String) -> void:
 	if added_sans % 2 == 0:
 		# warning-ignore:integer_division
 		create_number_label((added_sans / 2) + 1)
 	added_sans += 1
 	sans.get_children()[-1].add_move(move)
-	scroll_down()
 
 
 func add_moves(moves: PoolStringArray) -> void:
 	for move in moves:
 		add_move(move)
+	scroll_down()
 
 
 func scroll_down():
